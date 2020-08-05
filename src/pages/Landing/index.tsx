b729a26035abd705a1ba56aa5b9953763148d0b1
Alexandre <alehsilva@live.com>
2020-08-05 19:22:20 -0300

    Primeira requisição na API

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 
 import logoImg from '../../assets/images/logo.svg';
@@ -11,9 +11,21 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 
 import './styles.css'
+import api from '../../services/api';
 
 
 function Landing(){
+    const [totalConnections, setTotalConnections] = useState(0)
+
+    useEffect(() => {
+        api.get('connections').then(res => {
+            const {total} = res.data;
+
+            setTotalConnections(total);
+        })
+    }, []);
+
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -42,7 +54,7 @@ function Landing(){
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexções já realizadas 
+                    Total de {totalConnections} conexções já realizadas 
                     <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </span>
 
